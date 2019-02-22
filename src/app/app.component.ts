714c220d51d75257ec35dbe2429d2e43c6fc7954
djoue <djouejoel@gmail.com>
2019-02-22 17:39:01 +0000

    exercice  commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+	public conten = "Information : je vous conseille d'utiliser Bootstrap pour cet exercice.  Si vous créez des list-group-item dans un list-group, vous avez les classes list-group-item-success et list-group-item-danger pour colorer les item";
+	public post:any =[{
+					  title: "Mon premier post",
+					  content: this.conten,
+					  loveIts: 1,
+					  created_at: new Date().toLocaleString()
+					},{
+					  title: "Mon deuxieme post",
+					  content: this.conten,
+					  loveIts: 0,
+					  created_at: new Date().toLocaleString()
+					},{
+					  title: "Encore un post",
+					  content: this.conten,
+					  loveIts: -1,
+					  created_at: new Date().toLocaleString()
+					}];
   title = 'exo';
 }
